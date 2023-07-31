0da84bf7d477651f93a88aec274043571f74c156
Adam Burski <adam.burski@zeto.swidnica.pl>
2023-07-31 13:22:41 +0200

    Dodanie connection string

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using EFTraining.Models;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
