acf332de58a81d1a72d8d30b467e19acdd0814d8
Adam Burski <adam.burski@zeto.swidnica.pl>
2023-07-31 13:24:18 +0200

    Zastapienie tymczasowego zapisywania danych na polaczenie z baza danych

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using EFTraining.Models;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDb>(connectionString);
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
