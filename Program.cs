915cefddf9b3ebcf2de4d44aa2fa3a02b7550831
Adam Burski <adam.burski@zeto.swidnica.pl>
2023-07-31 12:49:39 +0200

    Dodanie aktualizacji pizzy

--- Program.cs
+++ Program.cs
@@ -29,5 +29,13 @@ app.MapPost("/pizza",async (PizzaDb db, Pizza pizza) =>
     await db.SaveChangesAsync();
     return Results.Created($"/pizza/{pizza.Id}",pizza);
 });
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) => {
+    var pizza = await db.Pizzas.FindAsync(id);
+    if(pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 
 app.Run();
